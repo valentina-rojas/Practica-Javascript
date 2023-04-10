907d52418c9280f9340471c645516593eb6b7d18
valentina-rojas <valentinarojas618@gmail.com>
2023-04-10 12:51:19 -0300

    nuevo archivo

---
+++ ejercicio02.js
@@ -0,0 +1,14 @@
+//Ingresar tres valores e indicar cual es el mayor y cual el menor.
+let a = 10;
+let b = 2;
+let c = 3;
+
+if (a > b && a > c) {
+  console.log("El numero mayor es: " + a);
+}
+if (b > a && b > c) {
+  console.log("El numero mayor es: " + b);
+}
+if (c > a && c > b) {
+  console.log("El numero mayor es: " + c);
+}
---
+++ js/ejercicio01.js
@@ -0,0 +1,6 @@
+// Ingresar dos valores y sumarlos.
+
+let a = 10;
+let b = 2;
+let suma = a + b;
+console.log("La suma es: " + suma);
